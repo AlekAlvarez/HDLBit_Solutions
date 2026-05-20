396c839f1204d5310b5596fd53125cf8459539d0
root <root@DESKTOP-NJ5E2AI.localdomain>
2026-05-20 11:55:02 -0500

    Fix syntax and logic issues

--- fadd.v
+++ fadd.v
@@ -2,11 +2,11 @@ module top_module (
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
-    wire cot,spare;
-    add16 o(a[15:0],b[15:0],'b0,sum[15,0],cot);
-    add16 t(a[31:16],b[31:16],'b0,sum[31:16],spare);
+    
 );//
-
+	wire cot,spare;
+    add16 o(a[15:0],b[15:0],1'b0,sum[15:0],cot);
+    add16 t(a[31:16],b[31:16],cot,sum[31:16],spare);
 endmodule
 
 module add1 ( input a, input b, input cin,   output sum, output cout );
